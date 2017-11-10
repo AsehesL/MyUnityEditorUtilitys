a366c130c80fb8ba4b51cfdbadfea95fba2f783e
AshesL <huancr@outlook.com>
2017-11-10 22:38:51 +0800

    lua模板创建代码

---
+++ Assets/Editor/ScriptsTemplate/LuaCreateUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class LuaCreateUtils
+{
+
+    [MenuItem("Assets/Create/Lua/MyLua.lua")]
+    static void CreateShader0000()
+    {
+        ProjectWindowUtilEx.CreateScriptUtil(@"Templates\Lua\MyLua.lua.txt", "New MyLua.lua");
+    }
+
+}
